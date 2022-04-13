3b6125d7b2b82ba731a31513b637f67b0383b96d
heartandkarma <heartandkarma@gmail.com>
2022-04-13 14:59:55 -0700

    Finished

--- hooks/useDarkMode.js
+++ hooks/useDarkMode.js
@@ -1,9 +1,9 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import useLocalStorage from "./useLocalStorage";
 
-function useDarkMode() {
-  const [darkMode, setDarkMode] = useState(false);
+const useDarkMode = () => {
+  const [darkMode, setDarkMode] = useLocalStorage("darkMode", false);
   return [darkMode, setDarkMode];
-}
+};
 
 export default useDarkMode;
--- hooks/useLocalStorage.js
+++ hooks/useLocalStorage.js
@@ -1,5 +1,14 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
-function useLocalStorage() {}
-
+function useLocalStorage(key, value) {
+  const [storedValue, setStoredValue] = useState(() => {
+    const item = window.localStorage.getItem(key);
+    return item ? JSON.parse(item) : value;
+  });
+  useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(storedValue));
+  }, [key, storedValue]);
+  return [storedValue, setStoredValue];
+}
 export default useLocalStorage;
